fd203683598186a04d847b0b6643e390df64aa5e
olegJS95 <Oleg.Holoshchapov@filiatix.com>
2023-10-10 19:21:42 +0300

    project in progress

--- src/pages/MainLanding/MainLanding.tsx
+++ src/pages/MainLanding/MainLanding.tsx
@@ -6,6 +6,19 @@ function MainLanding() {
         <div>
             <section className = {styles.topSection}>
                 <img src={image} className = {styles.girlsImg} alt='girls' />
+                <div className = {styles.topSectionInner}>
+                    <div className = {styles.topSectionRightColumn}>
+                        <div className = {styles.happyCouples}>
+                            <div className = {styles.happyCouplesQuantity}>
+                                2500+
+                            </div>
+                            Happy couples
+                        </div>
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
             </section>
         </div>
     )
