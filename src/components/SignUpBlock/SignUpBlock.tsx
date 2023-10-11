fd02c8135320dc132d52e2a9e60f501c0237e1bb
olegJS95 <Oleg.Holoshchapov@filiatix.com>
2023-10-11 17:26:08 +0300

    task in progress

--- src/components/SignUpBlock/SignUpBlock.tsx
+++ src/components/SignUpBlock/SignUpBlock.tsx
@@ -1,5 +1,7 @@
 import styles from "./SignUpBlock.module.scss"
 import {cc} from "../../utils/Classnames"
+import InputText from "../Input/InputText";
+import GreenButton from "../GreenButton/GreenButton";
 
 interface SignUpBlockProps {
 	className?: any
@@ -18,7 +20,24 @@ function SignUpBlock(props: SignUpBlockProps) {
 			)}
 
 		>
-			<h2>Sign up block</h2>
+			<h2 className={styles.signUpTitle}>sign up</h2>
+			<form className = {styles.formBlock}>
+				<div className = {styles.inputItemBlock}>
+					<InputText
+						type="text"
+						fullWidth="full"
+						placeholder={"user name or email"}
+					/>
+				</div>
+				<div className = {styles.inputItemBlock}>
+					<InputText
+						type="password"
+						fullWidth="full"
+						placeholder={"password"}
+					/>
+				</div>
+				<GreenButton>Create profile</GreenButton>
+			</form>
 		</div>
 	)
 }
