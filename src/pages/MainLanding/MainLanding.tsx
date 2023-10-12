7b671ea491bf7817c49fd6f31fc236cfd2191c46
olegJS95 <Oleg.Holoshchapov@filiatix.com>
2023-10-12 16:13:41 +0300

    task in progress

--- src/pages/MainLanding/MainLanding.tsx
+++ src/pages/MainLanding/MainLanding.tsx
@@ -7,30 +7,33 @@ import SignUpBlock from "../../components/SignUpBlock/SignUpBlock";
 function MainLanding() {
     return (
         <div>
-            <section className = {styles.topSection}>
-                <img src={image} className = {styles.girlsImg} alt='girls' />
-                <div className = {styles.topSectionInner}>
-                    <div className = {styles.topSectionRightColumn}>
-                        <div className = {styles.happyCouples}>
-                            <div className = {styles.happyCouplesQuantity}>
-                                2500+
+            <section className={styles.topSection}>
+                <img src={image} className={styles.girlsImg} alt='girls'/>
+                <div className={styles.topSectionInner}>
+                    <div className={styles.containerBlock}>
+                        <div className={styles.topSectionRightColumn}>
+                            <div className={styles.happyCouples}>
+                                <div className={styles.happyCouplesQuantity}>
+                                    2500+
+                                </div>
+                                Happy couples
                             </div>
-                            Happy couples
+                            <div className={styles.headerWithDescrAndButton}>
+                                <h1>Dating Ukrainian Single Ladies</h1>
+                                <p>
+                                    Join for free and start chatting
+                                </p>
+                                <p>
+                                    More than a thousand single women are ready for a serious relationship and are
+                                    waiting for your message
+                                </p>
+                                <GreenButton isNotFullWith>view profiles</GreenButton>
+                            </div>
                         </div>
-                        <div className = {styles.headerWithDescrAndButton}>
-                            <h1>Dating Ukrainian Single Ladies</h1>
-                            <p>
-                                Join for free and start chatting
-                            </p>
-                            <p>
-                                More than a thousand single women are ready for a serious relationship and are waiting for your message
-                            </p>
-                            <GreenButton isNotFullWith>view profiles</GreenButton>
-                        </div>
-                    </div>
-                    <div className = {styles.authColumn}>
-                        <div className = {styles.authColumnInner}>
-                             <SignUpBlock/>
+                        <div className={styles.authColumn}>
+                            <div className={styles.authColumnInner}>
+                                <SignUpBlock/>
+                            </div>
                         </div>
                     </div>
                 </div>
