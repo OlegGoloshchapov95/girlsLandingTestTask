130c45d2f43ee73a073d61799bc9a5df33da1726
olegJS95 <Oleg.Holoshchapov@filiatix.com>
2023-10-12 12:21:51 +0300

    task in progress

--- src/components/SignUpBlock/SignUpBlock.tsx
+++ src/components/SignUpBlock/SignUpBlock.tsx
@@ -3,45 +3,88 @@ import {cc} from "../../utils/Classnames"
 import InputText from "../Input/InputText";
 import GreenButton from "../GreenButton/GreenButton";
 import InputCheckboxHorizontal from "../CheckboxHorizontal/InputCheckboxHorizontal";
+import {Controller, useForm} from "react-hook-form";
 
 interface SignUpBlockProps {
-	className?: any
+    className?: any
+}
+
+type FormData = {
+    email: string
+    password: string
+    checkbox: boolean
 }
 
 function SignUpBlock(props: SignUpBlockProps) {
-	const {
-		className
-	} = props
+    const {
+        className
+    } = props
 
-	return (
-		<div
-			className={cc(
-				styles.signUpBlock,
-				className && className,
-			)}
+    const {handleSubmit, control} = useForm<FormData>({
+        mode: "onChange",
+    })
 
-		>
-			<h2 className={styles.signUpTitle}>sign up</h2>
-			<form className = {styles.formBlock}>
-				<div className = {styles.inputItemBlock}>
-					<InputText
-						type="text"
-						fullWidth="full"
-						placeholder={"user name or email"}
-					/>
-				</div>
-				<div className = {styles.inputItemBlock}>
-					<InputText
-						type="password"
-						fullWidth="full"
-						placeholder={"password"}
-					/>
-				</div>
-				<InputCheckboxHorizontal title = "hello, how are you?"/>
-				<GreenButton>Create profile</GreenButton>
-			</form>
-		</div>
-	)
+    const onFormSubmit = (data: FormData) => {
+        alert(JSON.stringify(data));
+    }
+
+    return (
+        <div
+            className={cc(
+                styles.signUpBlock,
+                className && className,
+            )}
+
+        >
+            <h2 className={styles.signUpTitle}>sign up</h2>
+            <form className={styles.formBlock} onSubmit={handleSubmit(onFormSubmit)}>
+                <div className={styles.inputItemBlock}>
+                    <Controller
+                        render={({field, fieldState}) => {
+                            return (
+                                <InputText
+                                    field = {field}
+                                    type="text"
+                                    fullWidth="full"
+                                    placeholder={"email"}
+                                />
+                            )
+                        }}
+                        name="email"
+                        control={control}
+                        defaultValue={""}
+                    />
+                </div>
+                <div className={styles.inputItemBlock}>
+                    <Controller
+                        render={({field, fieldState}) => {
+                            return (
+                                <InputText
+                                    field = {field}
+                                    type="password"
+                                    fullWidth="full"
+                                    placeholder={"password"}
+                                />
+                            )
+                        }}
+                        name="password"
+                        control={control}
+                        defaultValue={""}
+                    />
+                </div>
+                <Controller
+                    render={({field, fieldState}) => {
+                        return (
+                            <InputCheckboxHorizontal field = {field} title="hello, how are you?"/>
+                        )
+                    }}
+                    name="checkbox"
+                    control={control}
+                />
+                <GreenButton>Create profile</GreenButton>
+            </form>
+        </div>
+    )
 }
 
 export default SignUpBlock
